5c005f17af273a116155a92bcb71b1432d74ff97
tiagood <tiagood@Tiago>
2015-09-08 22:28:37 -0300

    Commit aula 03

---
+++ pessoas/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
@@ -0,0 +1,39 @@
+package br.unibh.pessoas.persistencia;
+
+import java.util.List;
+
+import br.unibh.pessoas.entidades.PessoaFisica;
+
+public class PessoaFisicaDAO implements DAO<PessoaFisica, Long> {
+
+	@Override
+	public PessoaFisica find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(PessoaFisica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PessoaFisica> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
